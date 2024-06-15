ede371c2020dc0dc015c9d2ebe64dcd04ba1683a
SpykeTiger <josevitorrequena@gmail.com>
2024-06-15 18:08:43 -0300

    Added pause menu

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -33,7 +33,7 @@ func _input(event):
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		$PauseMenu.pause()
 
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		if event is InputEventMouseMotion:
