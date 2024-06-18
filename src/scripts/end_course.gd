07ece94e79d8ec92ccc48d766ed761bc4ea6ca57
SpykeTiger <josevitorrequena@gmail.com>
2024-06-18 02:26:49 -0300

    Timer now resets, added Global file

--- src/scripts/end_course.gd
+++ src/scripts/end_course.gd
@@ -1,8 +1,9 @@
 extends Node3D
 class_name EndCourse
 
+@export var start_course: StartCourse
 
 func _on_body_entered(body):
 	if body is Player:
-		var timer = body.get_node("Neck/Camera3D/PanelContainer/Timer")
+		var timer = body.get_node("Neck/Timer").get_child(0).get_child(0)
 		timer.running = false
