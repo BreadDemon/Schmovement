c5681ffde6258455d46b23b029834a22811f9df8
SpykeTiger <josevitorrequena@gmail.com>
2024-06-23 20:57:18 -0300

    added keybinds save file

--- src/scripts/input_settings.gd
+++ src/scripts/input_settings.gd
@@ -23,10 +23,16 @@ var input_actions = {
 }
 
 func _ready():
+	_load_keybindings_from_settings()
 	_create_action_list()
 
+func _load_keybindings_from_settings():
+	var keybindings = ConfigFileHandler.load_keybindings()
+	for action in keybindings.keys():
+		InputMap.action_erase_events(action)
+		InputMap.action_add_event(action, keybindings[action])
+
 func _create_action_list():
-	InputMap.load_from_project_settings()
 	for item in action_list.get_children():
 		item.queue_free()
 
@@ -61,7 +67,10 @@ func _input(event):
 				event.double_click = false
 				
 			InputMap.action_erase_events(action_to_remap)
+			
 			InputMap.action_add_event(action_to_remap, event)
+			ConfigFileHandler.save_keybinding(action_to_remap, event)
+			
 			_update_action_list(remapping_button, event)
 			
 			is_remapping = false
@@ -74,6 +83,11 @@ func _update_action_list(button, event):
 	button.find_child("LabelInput").text = event.as_text().trim_suffix(" (Physical)")
 
 func _on_reset_button_pressed():
+	InputMap.load_from_project_settings()
+	for action in input_actions:
+		var events = InputMap.action_get_events(action)
+		if events.size > 0:
+			ConfigFileHandler.save_keybinding(action, events[0])
 	_create_action_list()
 
 func _on_back_button_pressed():
